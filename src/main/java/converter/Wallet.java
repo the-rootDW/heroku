acffd7cafc9fe372ad728923f09819e320002bbe
the-rootDW <34452597+the-rootDW@users.noreply.github.com>
2018-04-30 02:09:00 +0300

    Add files via upload

---
+++ src/main/java/converter/Wallet.java
@@ -0,0 +1,44 @@
+package converter;
+
+public enum Wallet {
+    QIWI {
+        public String toString() {
+            return "+380992077771";
+        }
+    },
+    WM {
+        public String toString() {
+            return "R312037427779";
+        }
+    },
+    YANDEX {
+        public String toString() {
+            return "4149980802898099";
+        }
+    },
+    PAYEER {
+        public String toString() {
+            return "R485930";
+        }
+    },
+    BTC {
+        public String toString() {
+            return "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa";
+        }
+    },
+    LTC {
+        public String toString() {
+            return "38Khkhuh7uygYGuyg887hu7huib78оkKl";
+        }
+    },
+    BCC {
+        public String toString() {
+            return "0xca30e63200a0fe3182dc61fc5605efc41456f32";
+        }
+    },
+    ETH {
+        public String toString() {
+            return "0xca30e63200a0fe3182dc61fc5605efc41456f32";
+        }
+    }
+}
